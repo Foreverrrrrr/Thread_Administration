80efc6423b1180142b2d81c0bd518cfe4c352649
Forever . <1727365097@qq.com>
2024-01-26 14:50:15 +0800

    Thread_Auto_Base新增加NewClass_Run多线程启动完成事件，可以通过该事件来获取子类中的实例对象以及子类中的Run_Log订阅触发也可以通过该事件来处理

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ConsoleApp1
+{
+    using System;
+    using System.Reflection;
+    using System.Reflection.Emit;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            TypeBuilder typeBuilder = CreateDynamicType();
+            MethodInfo dynamicMethod = typeBuilder.GetMethod("Run");
+
+            dynamicMethod.Invoke(null, null);
+            Run();
+        }
+
+        public static TypeBuilder CreateDynamicType()
+        {
+            // 创建程序集和模块
+            AssemblyName assemblyName = new AssemblyName("ConsoleApp1");
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("ConsoleApp1");
+
+            // 创建类型
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("ConsoleApp1", TypeAttributes.Public);
+            MethodBuilder methodBuilder = typeBuilder.DefineMethod("Run", MethodAttributes.Public | MethodAttributes.Static, typeof(void), new Type[0]);
+
+            // 获取ILGenerator
+            ILGenerator ilGenerator = methodBuilder.GetILGenerator();
+
+            // 在每行代码后注入指定方法
+            //InjectMethodAfterEachLine(ilGenerator, typeof(Program).GetMethod("CustomMethod"));
+
+            // 完成方法
+            ilGenerator.Emit(OpCodes.Ret);
+
+            // 创建类型
+            Type dynamicType = typeBuilder.CreateType();
+
+            return typeBuilder;
+        }
+
+        public static void CustomMethod()
+        {
+            Console.WriteLine("Custom method");
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine("21");
+            Console.WriteLine("21");
+            Console.WriteLine("21");
+            Console.WriteLine("21");
+            Console.WriteLine("21");
+            Console.ReadLine();
+        }
+
+
+        public static OpCode GetOpCode(byte opcodeValue)
+        {
+            FieldInfo[] opCodeFields = typeof(OpCodes).GetFields(BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var field in opCodeFields)
+            {
+                OpCode opCode = (OpCode)field.GetValue(null);
+
+                if (opCode.Value == opcodeValue)
+                {
+                    return opCode;
+                }
+            }
+
+            throw new ArgumentException("Invalid opcode value.");
+        }
+    }
+}
---
+++ Thread_Administration/Base/Auto_Thread.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Thread_Administration.Base
+{
+    public abstract class Thread_Auto_Base
+    {
+        public abstract event Action<DateTime, string> Run_Log;
+
+        public static event Action<DateTime> NewClass_Run;
+
+        public static System.Collections.Generic.Queue<ProductionThreadBase> Auto_Th { get; set; }
+
+        public Thread_Auto_Base()
+        {
+            Auto_Th = new System.Collections.Generic.Queue<ProductionThreadBase>();
+        }
+
+        public static void NewClass(int spintime = 50)
+        {
+            Type baseType = typeof(Thread_Auto_Base);
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] derivedTypes = assembly.GetTypes().Where(t => t.IsSubclassOf(baseType)).ToArray();
+            foreach (Type derivedType in derivedTypes)
+            {
+                object instance = Activator.CreateInstance(derivedType);
+                MethodInfo[] methods = derivedType.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+                foreach (MethodInfo method in methods)
+                {
+                    var t = method.GetCustomAttributes(typeof(ProductionThreadBase), inherit: true);
+                    if (t.Length > 0)
+                        Thread_Configuration(derivedType.Name, method, instance, spintime);
+                }
+            }
+            NewClass_Run?.Invoke(DateTime.Now);
+        }
+
+        private static void Thread_Configuration(string class_na, MethodInfo method, object class_new, int spintime)
+        {
+            Thread_Auto_Base instance = class_new as Thread_Auto_Base ?? throw new Exception("Automatic thread conversion exception");
+            DescriptionAttribute descriptionAttribute = (DescriptionAttribute)method.GetCustomAttribute(typeof(DescriptionAttribute));
+            ProductionThreadBase threadBase = new ProductionThreadBase()
+            {
+                Target = method.Name,
+                Thread_Name = descriptionAttribute?.Description ?? method.Name,
+                New_Thread = new Thread(() => { while (true) { Thread.Sleep(spintime); method.Invoke(instance, new object[] { instance }); } }),
+            };
+            threadBase.New_Thread.Name = class_na + "." + threadBase.Thread_Name;
+            threadBase.New_Thread.IsBackground = true;
+            threadBase.New_Thread.Start();
+            Auto_Th?.Enqueue(threadBase);
+        }
+
+        [ProductionThreadBase]
+        public abstract void Initialize(object thread);
+
+        [ProductionThreadBase]
+        public abstract void Main(object thread);
+
+        [ProductionThreadBase]
+        public abstract void Error(object thread);
+    }
+}
