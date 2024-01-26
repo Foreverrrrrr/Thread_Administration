e9a429a9c796191e1cc8cb76873e117ade3cc511
Forever . <1727365097@qq.com>
2024-01-26 14:50:15 +0800

    添加项目文件。

---
+++ Thread_Administration/Base/Exchange.cs
@@ -0,0 +1,24 @@
+﻿namespace Thread_Administration.Base
+{
+    public class Exchange
+    {
+        public byte[] Input { get; set; }
+        public byte[] Output { get; set; }
+        public bool[] Error { get; set; }
+
+        public enum Send_Input : ushort
+        {
+            a, b, c, d, e, f, g,
+        }
+
+        public enum Send_Output : ushort
+        {
+
+        }
+
+        public enum Send_Error : ushort
+        {
+
+        }
+    }
+}
---
+++ Thread_Administration/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Thread_Administration.Base;
+
+namespace Thread_Administration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Thread_Auto_Base.NewClass();
+            InitializeComponent();
+            //MyClass myClass = new MyClass();
+            Thread_Auto_Base.NewClass();
+        }
+    }
+
+
+    public class MyClass : Thread_Auto_Base
+    {
+        protected override event Action<DateTime, string> Run_Log;
+
+
+        public MyClass() : base()
+        {
+
+        }
+
+        public override void Error(object thread)
+        {
+
+        }
+
+        public override void Initialize(object thread)
+        {
+
+        }
+
+        
+        public override void Main(object thread)
+        {
+
+        }
+    }
+
+    class MyClass1 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+        }
+    }
+
+    class MyClass2 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+        }
+    }
+
+    class MyClass3 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+        }
+    }
+
+    class MyClass4 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+
+        }
+    }
+
+    class MyClass5 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+        }
+    }
+
+
+    class MyClass6 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+        }
+    }
+    class MyClass7 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+
+        }
+    }
+
+    class MyClass8 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+
+        }
+    }
+    class MyClass9 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+
+        }
+    }
+
+    class MyClass10 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+
+        }
+    }
+
+    class MyClass11: Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+
+        }
+    }
+
+    class MyClass12 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+
+        }
+    }
+
+    class MyClass13 : Thread_Auto_Base
+    {
+
+        protected override event Action<DateTime, string> Run_Log;
+
+        public override void Error(object thread)
+        {
+        }
+
+        public override void Initialize(object thread)
+        {
+        }
+
+        public override void Main(object thread)
+        {
+
+        }
+    }
+}
